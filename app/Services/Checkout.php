c02db52e9d732e853de8aff834b6f7398443c96f
Geshan Manandhar <geshan@gmail.com>
2016-09-24 08:58:36 +0400

    Adds a feature flag/switch for PayPal payment method
    - [x] Adds a payment method `PayPal`
    - [x] Paypal is only available for `@gmail.com` emails
    - [x] Changes the tests for the same

--- app/Services/Checkout.php
+++ app/Services/Checkout.php
@@ -18,12 +18,16 @@ class Checkout
     /**
      * @var float
      */
-    protected $subTotal          = 0.0;
+    protected $subTotal = 0.0;
 
     /**
      * @var string
      */
     protected $paymentMethod;
+
+    const ALLOWED_PAYMENT_METHODS = ['Cash', 'CreditCard', 'PayPal'];
+
+    const PAYAPL_ALLLOWED_DOMAIN = '@gmail.com';
 
     const CASH_ON_DELIVERY_FEE = 5.00;
 
@@ -31,16 +35,35 @@ class Checkout
      * Calculates total, if payment method is cash 5.00 is added as
      * post payment fees.
      *
+     * If payment method is invalid, will return 0.
+     *
+     * Payment method `PayPal` has a feature switch/flag, it is available ony for email ending in @gmail.com.
+     *
      * @param $paymentMethod
      * @return float
      */
-    public function calculateTotal($paymentMethod)
+    public function calculateTotal($paymentMethod, $email)
     {
+        $invalidOrderTotal = 0;
+
+        if (!$this->isPaymentMethodValid($paymentMethod)) {
+            return $invalidOrderTotal;
+        }
+
+        if ('PayPal' === $paymentMethod && !ends_with($email, self::PAYAPL_ALLLOWED_DOMAIN)) {
+            return $invalidOrderTotal;
+        }
+
         $this->subTotal = 95.00;
         if ('Cash' === $paymentMethod) {
             $this->cashOnDeliveryFee = self::CASH_ON_DELIVERY_FEE;
         }
 
         return $this->subTotal + $this->cashOnDeliveryFee;
+    }
+
+    protected function isPaymentMethodValid($paymentMethod)
+    {
+        return in_array($paymentMethod, self::ALLOWED_PAYMENT_METHODS, true);
     }
 }
--- tests/Integrated/ExampleTest.php
+++ tests/Integrated/ExampleTest.php
@@ -23,10 +23,15 @@ class ExampleTest extends TestCase
     public function providerAllUrisWithResponseCode()
     {
         return [
-            ['/', 200],
-            ['/place/cc', 200],
-            ['/place/cod', 200],
-            ['/non-existing', 404],
+            ['/', 200, 'Laravel 5'],
+            ['/place/CreditCard', 200, 'Checkout for CreditCard with total 95'],
+            ['/place/CreditCard/me@mydomain.com', 200, 'Checkout for CreditCard with total 95'],
+            ['/place/Cash', 200, 'Checkout for Cash with total 100'],
+            ['/place/Cash/me@mydomain.com', 200, 'Checkout for Cash with total 100'],
+            ['/place/PayPal/myName@gmail.com', 200, 'Checkout for PayPal with total 95'],
+            ['/place/PayPal/me@mydomain.com', 200, 'Payment method PayPal, is not valid.'],
+            ['/place/iDeal/me@mydomain.com', 200, 'Payment method iDeal, is not valid.'],
+            ['/non-existing', 404, 'the page you are looking for could not be found.'],
         ];
     }
 
@@ -35,11 +40,12 @@ class ExampleTest extends TestCase
     *
     * @dataProvider providerAllUrisWithResponseCode
     **/
-    public function testApplicationUriResponses($uri, $responseCode)
+    public function testApplicationUriResponses($uri, $responseCode, $visibleText)
     {
-      print sprintf('checking URI : %s - to be %d - %s', $uri, $responseCode, PHP_EOL);
-      $response = $this->call('GET', $uri);
+        print sprintf('checking URI : %s - to be %d - %s', $uri, $responseCode, PHP_EOL);
+        $response = $this->call('GET', $uri);
 
-      $this->assertEquals($responseCode, $response->status());
+        $this->assertEquals($responseCode, $response->status());
+        $this->see($visibleText);
     }
 }
